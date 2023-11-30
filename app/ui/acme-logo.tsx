43390809bc2ec3bbed6094d063655473cc0721f7
androol <multiad05@gmail.com>
2023-12-01 08:10:09 +0900

    8th

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { CubeIcon } from '@heroicons/react/24/outline';
 import { roboto_mono } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${roboto_mono.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <CubeIcon className="h-6 w-6 rotate-[15deg]" />
+      <p className="ml-2 text-[16px]">KMS School</p>
     </div>
   );
 }
