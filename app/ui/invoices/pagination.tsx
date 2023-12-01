9fc003e084dac72920ae85aba50249742a1ad639
androol <multiad05@gmail.com>
2023-12-01 11:13:09 +0900

    10th

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
@@ -113,9 +113,9 @@ function PaginationArrow({
 
   const icon =
     direction === 'left' ? (
-      <ArrowLeftIcon className="w-4" />
+      <ChevronLeftIcon className="w-4" />
     ) : (
-      <ArrowRightIcon className="w-4" />
+      <ChevronRightIcon className="w-4" />
     );
 
   return isDisabled ? (
