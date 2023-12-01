72d6b51a35c1ea544adcce4e6ca7bb21292f3a1c
androol <multiad05@gmail.com>
2023-12-01 15:41:41 +0900

    11th

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
+import { ChevronLeftIcon, ChevronRightIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
@@ -24,12 +24,16 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
       {/* NOTE: comment in this code when you get to this point in the course */}
 
       <div className="inline-flex">
+        <DoubleArrow
+          direction="first"
+          href={createPageURL(1)}
+          isDisabled={currentPage <= 1}
+        />
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
           isDisabled={currentPage <= 1}
         />
-
         <div className="flex -space-x-px">
           {allPages.map((page, index) => {
             let position: 'first' | 'last' | 'single' | 'middle' | undefined;
@@ -54,6 +58,11 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
         <PaginationArrow
           direction="right"
           href={createPageURL(currentPage + 1)}
+          isDisabled={currentPage >= totalPages}
+        />
+        <DoubleArrow
+          direction="last"
+          href={createPageURL(totalPages)}
           isDisabled={currentPage >= totalPages}
         />
       </div>
@@ -116,6 +125,41 @@ function PaginationArrow({
       <ChevronLeftIcon className="w-4" />
     ) : (
       <ChevronRightIcon className="w-4" />
+    );
+
+  return isDisabled ? (
+    <div className={className}>{icon}</div>
+  ) : (
+    <Link className={className} href={href}>
+      {icon}
+    </Link>
+  );
+}
+
+function DoubleArrow({
+  href,
+  direction,
+  isDisabled,
+}: {
+  href: string;
+  direction: 'first' | 'last';
+  isDisabled?: boolean;
+}) {
+  const className = clsx(
+    'flex h-10 w-10 items-center justify-center rounded-md border',
+    {
+      'pointer-events-none text-gray-300': isDisabled,
+      'hover:bg-gray-100': !isDisabled,
+      'mr-2 md:mr-4': direction === 'first',
+      'ml-2 md:ml-4': direction === 'last',
+    },
+  );
+
+  const icon =
+    direction === 'first' ? (
+      <ChevronDoubleLeftIcon className="w-4" />
+    ) : (
+      <ChevronDoubleRightIcon className="w-4" />
     );
 
   return isDisabled ? (
